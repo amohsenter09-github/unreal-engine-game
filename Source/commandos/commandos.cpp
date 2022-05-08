aae3fd8a530a87c1223d570259a443bc6361e577
amohsenter09-github <64581648+amohsenter09-github@users.noreply.github.com>
2022-05-08 20:45:55 +0200

    This is the first commit of unreal engine
    This is the main commit of unreal engine

---
+++ Source/commandos/commandos.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "commandos.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, commandos, "commandos" );
---
+++ Source/commandos/commandosGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "commandosGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMMANDOS_API AcommandosGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
